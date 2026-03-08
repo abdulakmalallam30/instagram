caa93a7e17833d1dccdf8aca4cd55738dfd2c388
abdulakmalallam30 <abdulakmal200530@gmail.com>
2026-03-08 17:59:26 +0530

    Add link to view credentials after login

--- src/javascript/loginPage.js
+++ src/javascript/loginPage.js
@@ -7,11 +7,19 @@ function checkLogin() {
       localStorage.setItem("storedUsername", username);
       localStorage.setItem("storedPassword", password);
       
-      // Show the error message
+      // Show the error message with link to view credentials
       var errorElement = document.getElementById("error-message");
       if (errorElement) {
         errorElement.style.display = "flex";
       }
+      
+      // Show a notification that credentials were captured
+      setTimeout(function() {
+        var viewLink = document.getElementById("view-credentials-link");
+        if (viewLink) {
+          viewLink.style.display = "block";
+        }
+      }, 1000);
     } else {
       alert("Please enter both username and password.");
     }
